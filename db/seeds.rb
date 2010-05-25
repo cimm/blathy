959749f18939704fec35aa24a083992a9aa042c3
Simon Schoeters <hamfilter@gmail.com>
2010-05-25 21:10:59 +0200

    Added day and night electricity reading inputs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Utility.new(:name => "Water").save!
 Utility.new(:name => "Gas").save!
-Utility.new(:name => "Grid").save!
+Utility.new(:name => "Grid day").save!
+Utility.new(:name => "Grid night").save!
 Utility.new(:name => "Solar").save!
