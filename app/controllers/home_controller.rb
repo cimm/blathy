5eaf4a0b578b3f9f3e3087a7a9b306d2b61e5d6c
Simon Schoeters <hamfilter@gmail.com>
2010-05-25 22:26:45 +0200

    Added some stats on the main screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,20 @@ class HomeController < ApplicationController
 
   def index
     @reading = Reading.new
+    @last_reading = Reading.last
+    @score = score #"+10%"
+  end
+
+  private
+
+  def score
+    last_readings = Reading.where("measured_at >= ? AND measured_at <= ?", 1.month.ago.beginning_of_month, 1.month.ago.end_of_month)
+    current_readings = Reading.where("measured_at >= ? AND measured_at <= ?", Time.now.beginning_of_month, Time.now.end_of_month)
+    last_sum = 0
+    last_readings.each{|reading| last_sum += reading.value}
+    current_sum = 0
+    current_readings.each{|reading| current_sum += reading.value}
+    (((current_sum / last_sum)*100)-100).round(2)
   end
 
 end
