862f87c7ed42c233b1e6aa4c2d325b50105d92aa
Simon Schoeters <hamfilter@gmail.com>
2010-05-25 20:47:20 +0200

    Moved the input screens to a partial

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,6 +2,7 @@ class ReadingsController < ApplicationController
 
   def create
     @reading = Reading.new(params[:reading])
+    @reading.utility_id = Utility.find_by_name(params[:reading][:utility]).id
     if @reading.save
       render :file => 'readings/create'
     end
