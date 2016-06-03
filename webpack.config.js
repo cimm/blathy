b345f6212f3c783a4736914b19f411dadf90b0c4
Simon Schoeters <hamfilter@gmail.com>
2016-06-03 18:55:42 +0200

    Fix JSX parsing with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
     ]
   },
   plugins: [HTMLWebpackPluginConfig]
