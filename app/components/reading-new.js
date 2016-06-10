a9bfd6b791716403f110d845545ffe3b665cafcb
Simon Schoeters <hamfilter@gmail.com>
2016-06-10 23:16:04 +0200

    Add reading chart

--- app/components/reading-new.js
+++ app/components/reading-new.js
@@ -1,5 +1,9 @@
 var React = require('react');
 var Store = require('../store.js');
+var Victory = require('victory');
+var VictoryChart = Victory.VictoryChart;
+var VictoryLine = Victory.VictoryLine;
+var VictoryScatter = Victory.VictoryScatter;
 var ReadingList = require('./reading-list.js');
 var ReadingForm = require('./reading-form.js');
 
@@ -23,15 +27,37 @@ var ReadingNew = React.createClass({
         var utilityReadings = res.data.filter(reading => {
           return reading.utilityId == this.props.params.id;
         });
+        utilityReadings.sort((a, b) => a.readAt > b.readAt)
         this.setState({readings: utilityReadings});
       });
   },
 
   render: function() {
+    var lineData = [];
+    for (let reading of this.state.readings) {
+      lineData.push({x: reading.readAt, y: reading.volume});
+    }
+    if(lineData.length > 1) {
+      var chart = [
+        <VictoryChart key='chart' scale={{x: 'time'}}>
+          <VictoryLine data={lineData}/>
+          <VictoryScatter
+            data={lineData}
+            style={{
+              data: {
+                stroke: 'white',
+                strokeWidth: 3
+              }}
+            }
+          />
+        </VictoryChart>
+      ];
+    }
     return (
       <div>
         <h1>{this.props.params.id}</h1>
         <ReadingList readings={this.state.readings}/>
+        {chart}
         <ReadingForm utilityId={this.props.params.id} />
       </div>
     )
