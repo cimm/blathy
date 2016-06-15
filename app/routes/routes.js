2b5f2700542dd15b5defd952c92ae716343719f0
Simon Schoeters <hamfilter@gmail.com>
2016-06-15 22:32:35 +0200

    Use hashHistory
    Adds junk to the URL but does not need server-side configuration.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,13 +2,13 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var browserHistory = ReactRouter.browserHistory;
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/main');
 var ReadingNew = require('../components/reading/new');
 var UtilityNew = require('../components/utility/new');
 
 var routes = (
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}/>
     <Route path='/utilities/new' component={UtilityNew}/>
     <Route path='/utilities/:id/readings/new' component={ReadingNew}/>
