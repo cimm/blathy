5987ea381e734737fa91419c9cdc0a29e67db223
Simon Schoeters <hamfilter@gmail.com>
2019-11-03 22:43:08 +0100

    Correctly deserialize dates in JSON

--- src/store.js
+++ src/store.js
@@ -1,5 +1,15 @@
 import { writable } from 'svelte/store'
 
+const dateFormat = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/
+
+// JSON has no date type, we need to desrialize manually
+function dateParser(key, val) {
+  if (typeof val === 'string' && dateFormat.test(val)) {
+    return new Date(val)
+  }
+  return val
+}
+
 function createUtilities() {
   const key = 'utilities'
   const storedUtilities = JSON.parse(localStorage.getItem(key))
@@ -16,7 +26,7 @@ function createUtilities() {
 
 function createReadings() {
   const key = 'readings'
-  const storedReadings = JSON.parse(localStorage.getItem(key))
+  const storedReadings = JSON.parse(localStorage.getItem(key), dateParser)
   const { subscribe, set, update } = writable(storedReadings || [])
 
   return {
