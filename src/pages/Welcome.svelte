9133e0e98c56c7ba7416e59caafc2b53231a38c2
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2020-01-26 16:26:29 +0100

    Use new size attribute for icons

--- src/pages/Welcome.svelte
+++ src/pages/Welcome.svelte
@@ -13,20 +13,15 @@
     margin: 1em auto;
     width: 2em;
   }
-  span {
-    display: inline-block;
-    vertical-align: middle;
-    width: 1em;
-  }
 </style>
 
 <section>
   <h1><CheckCircleIcon/></h1>
   <p>Bláthy is a tool to keep track of your energy usage.</p>
   <ol>
-    <li>Create one or more utilities (eg. water, electricity or gas) to track via the <span><PlusSquareIcon/></span> icon or import a previous backup via the <span><UploadIcon/></span> icon.</li>
+    <li>Create one or more utilities (eg. water, electricity or gas) to track via the <PlusSquareIcon size="1x"/> icon or import a previous backup via the <UploadIcon size="1x"/> icon.</li>
     <li>Record the value of your utility meter as often as you like (eg. once per month).</li>
     <li>Get an idea of your energy usage by inspecting the graphs.</li>
   </ol>
-  <p>Bláthy is a progressive web app (= a website behaving as a mobile app). It works without internet connection and does not store any information outside this device. Take regular backups via the <span><DownloadIcon/></span> icon!</p>
+  <p>Bláthy is a progressive web app (= a website behaving as a mobile app). It works without internet connection and does not store any information outside this device. Take regular backups via the <DownloadIcon size="1x"/> icon!</p>
 </section>
